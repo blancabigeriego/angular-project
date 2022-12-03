994633976ae7f1f4049c1e1abe756c12433cda0a
Blanca <blancabcbt@gmail.com>
2022-12-03 20:11:52 +0100

    create action for loading and load characters

---
+++ project/src/app/actions/characters.action.ts
@@ -0,0 +1,10 @@
+
+import { createAction, props } from "@ngrx/store";
+import { Character } from "../models/character.model";
+
+export const loadingCharacters = createAction( '[Characters] Loading Characters',);
+
+export const loadCharacters =
+createAction('[Characters] Load Characters', props<{ characters: Character[] }>()
+);
+
