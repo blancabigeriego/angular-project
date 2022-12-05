20460b7474090b2776328d1cabf9f6942c382402
Blanca <blancabcbt@gmail.com>
2022-12-05 10:37:17 +0100

    update the app state with new reducer

--- project/src/app/state/app.state.ts
+++ project/src/app/state/app.state.ts
@@ -1,10 +1,15 @@
 import { ActionReducerMap } from "@ngrx/store";
 import { charactersReducer, CharacterState } from "../reducers/characters.reducer";
+import { characterDetailsReducer, CharacterDetailState } from "../reducers/character-details.reducer";
+
 
 export interface AppState {
     charactersState: CharacterState;
+    characterDetailState: CharacterDetailState;
+
 };
 
 export const ROOT_REDUCERS: ActionReducerMap<AppState> = {
     charactersState: charactersReducer,
+    characterDetailState: characterDetailsReducer
 };
