b8e87995f0fa9bcc32afc656489d7533db42142b
Blanca <blancabcbt@gmail.com>
2022-12-05 09:35:49 +0100

    add selectors to get data

---
+++ project/src/app/state/selector/characters.selector.ts
@@ -0,0 +1,19 @@
+import { createSelector } from '@ngrx/store';
+import { AppState } from '../app.state';
+import { charactersReducer, CharacterState } from 'src/app/reducers/characters.reducer';
+
+export const selectCharacterState = (state: AppState) => state.charactersState;
+
+export const selectCharacters = createSelector(
+    selectCharacterState,
+    (state: CharacterState)=> {
+        return state.characters;
+    }
+);
+
+export const selectLoading = createSelector(
+    selectCharacterState,
+    (state: CharacterState) => {
+        return state.loading;
+    }
+);
