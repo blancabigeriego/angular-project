299c4388060d0c5fa28662864bd1fd2e704c479c
Blanca <blancabcbt@gmail.com>
2022-12-07 10:18:14 +0100

    add new delete petition in service

--- project/src/app/state/actions/delete-character.action.ts
+++ project/src/app/state/actions/delete-character.action.ts
@@ -1,3 +1,7 @@
 import { createAction, props } from "@ngrx/store";
 import { Character } from "src/app/models/character.model";
 
+export const deleteCharacter = createAction(
+    '[Delete Character] Delete Character',
+    props<{ character: Character}>()
+)
